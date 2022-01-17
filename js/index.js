1c8c6434bd4f78c1e6ba8f173bb2fafb441a8940
John Doe <alirezanjprogrammer110@gmail.com>
2022-01-17 08:15:49 +0330

    function show module and close module and edit module

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
+
 // btn show module
 const btn_show = document.querySelector(".btn-show");
 const module = document.querySelector(".module"); //module
-const btnEdit=document.querySelector(".btn-edit")
-btn_show.addEventListener("click", (event) => {
+const btnEdit = document.querySelector(".btn-edit");
+const btnClose = document.querySelector(".btn-close");
+const btnAdd = document.querySelector(".btn-add");
+
+btn_show.addEventListener("click", ShowModule);
+btnClose.addEventListener("click", CloseModule);
+btnEdit.addEventListener("click", ShowModuleEdit);
+
+// close module
+function CloseModule() {
+  module.classList.remove("flex");
+  module.classList.add("hidden");
+  module.children[0].innerText = "new todo";
+  btnAdd.innerText="add";
+
+
+}
+// show module
+function ShowModule() {
   module.classList.remove("hidden");
   module.classList.add("flex");
-});
+}
+// shomodule edit
+function ShowModuleEdit() {
+  module.children[0].innerText = "Edit todo";
+  btnAdd.innerText="edit";
+  ShowModule();
 
+}
