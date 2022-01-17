7aed89c383cc3eda9b290fe6ec08044ed466aeb8
John Doe <alirezanjprogrammer110@gmail.com>
2022-01-17 18:50:02 +0330

    add todo in todolist and remove, complid, edit1

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-
 // btn show module
 const btn_show = document.querySelector(".btn-show");
 const module = document.querySelector(".module"); //module
-const btnEdit = document.querySelector(".btn-edit");
 const btnClose = document.querySelector(".btn-close");
 const btnAdd = document.querySelector(".btn-add");
+const inputBox = document.querySelector(".input-box");
+const todolist = document.querySelector(".todolist");
+const btnEdit = document.querySelectorAll(".btn-edit");
 
 btn_show.addEventListener("click", ShowModule);
 btnClose.addEventListener("click", CloseModule);
-btnEdit.addEventListener("click", ShowModuleEdit);
+btnAdd.addEventListener("click", AddTodo);
+todolist.addEventListener("click", editanddlite);
 
 // close module
 function CloseModule() {
   module.classList.remove("flex");
   module.classList.add("hidden");
-  module.children[0].innerText = "new todo";
-  btnAdd.innerText="add";
-
-
+  SetTitleModule();
 }
 // show module
 function ShowModule() {
@@ -25,9 +24,58 @@ function ShowModule() {
   module.classList.add("flex");
 }
 // shomodule edit
-function ShowModuleEdit() {
+function ShowModuleEdit(e) {
   module.children[0].innerText = "Edit todo";
-  btnAdd.innerText="edit";
+  btnAdd.innerText = "edit";
   ShowModule();
+}
+// set title module edite and add
+function SetTitleModule() {
+  module.children[0].innerText = "new todo";
+  btnAdd.innerText = "add";
+}
+// add todo in html
+function AddTodo() {
+  const todoItem = document.createElement("li");
+  let result = "";
+  result = `
+<!-- items todo -->
+<li class="todo  ">
+    <!-- content and chekbox -->
+    <span class="flex items-center text-2xl">
+        <input type="checkbox" class="form-checkbox rounded-full">
 
+        <p class="text">
+        ${inputBox.value}
+        </p>
+    </span>
+    <span class="text-2xl">
+        <i class="btn-edit fas fa-pencil-alt text-blue-500"></i>
+        <i class=" fa fa-trash text-red-600 ml-4" aria-hidden="true"></i>
+    </span>
+</li>`;
+  todoItem.innerHTML = result;
+
+  todolist.appendChild(todoItem);
+  inputBox.value = "";
+}
+
+function editanddlite(e) {
+  const text_todo = document.querySelectorAll(".text");
+
+  const item = e.target;
+  if (item.classList[0] === "btn-edit") {
+    ShowModuleEdit();
+    inputBox.value = item.parentElement.parentElement.innerText;
+  } else if (item.classList[1] === "fa-trash") {
+    const todo = item.parentElement.parentElement;
+    todo.remove();
+  } else if (item.classList[0] === "form-checkbox") {
+    if (item.checked == true) {
+      item.parentElement.classList.toggle("complid")
+    }
+    else{
+      item.parentElement.classList.remove("complid")
+    }
+  }
 }
