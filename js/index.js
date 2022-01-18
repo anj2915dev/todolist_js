3371a60991eb0be18a5f2ce4e123bd38552d0dc0
John Doe <alirezanjprogrammer110@gmail.com>
2022-01-18 11:58:29 +0330

    date and readcumplid

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// btn show module
+
 const btn_show = document.querySelector(".btn-show");
 const module = document.querySelector(".module"); //module
 const btnClose = document.querySelector(".btn-close");
@@ -7,12 +7,22 @@ const inputBox = document.querySelector(".input-box");
 const todolist = document.querySelector(".todolist");
 const btnEdit = document.querySelectorAll(".btn-edit");
 const dateDom = document.querySelectorAll(".date");
-
+const btnCumplid = document.querySelector(".btn-cumplid");
+const btnOnCumplid = document.querySelector(".btn-oncumplid");
+const day = document.querySelector(".day");
+const month = document.querySelector(".month");
+const year = document.querySelector(".year");
+//  start controls
 btn_show.addEventListener("click", ShowModule);
 btnClose.addEventListener("click", CloseModule);
 btnAdd.addEventListener("click", AddTodo);
 todolist.addEventListener("click", editanddlite);
+btnCumplid.addEventListener("click", redCumplid);
+btnOnCumplid.addEventListener("click", redAll);
 document.querySelector("DOMContentLoaded", getTodo());
+//  end controls
+
+// start function
 
 // close module
 function CloseModule() {
@@ -73,12 +83,25 @@ function editanddlite(e) {
     inputBox.value = item.parentElement.parentElement.innerText;
   } else if (item.classList[1] === "fa-trash") {
     const todo = item.parentElement.parentElement;
+    removeLocultodos(todo);
+
     todo.remove();
   } else if (item.classList[0] === "form-checkbox") {
     if (item.checked == true) {
-      item.parentElement.classList.toggle("complid");
+      item.parentElement.classList.add("complid");
+
+      item.parentElement.parentElement.parentElement.classList.add("iscumplid");
+      item.parentElement.parentElement.children[1].children[0].classList.add(
+        "hidden"
+      );
     } else {
       item.parentElement.classList.remove("complid");
+      item.parentElement.parentElement.children[1].children[0].classList.remove(
+        "hidden"
+      );
+      item.parentElement.parentElement.parentElement.classList.remove(
+        "iscumplid"
+      );
     }
   }
 }
@@ -121,10 +144,38 @@ function getTodo() {
   });
   date();
 }
+// remove from localstorage
+function removeLocultodos(y) {
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+  const filterTodos = savedTodos.filter((i) => i !== y.children[0].innerText);
+  localStorage.setItem("todos", JSON.stringify(filterTodos));
+}
+// read todo class cumplid
+function redCumplid() {
+  const itemtodo = todolist.children;
+  console.log(itemtodo);
+  const todoItem = [...itemtodo];
+  todoItem.forEach((i) => {
+    if (!i.classList.contains("iscumplid")) {
+      i.style.display = "none";
+    }
+  });
+}
+// read todos
+function redAll() {
+  const itemtodo = todolist.children;
 
+  const todoItem = [...itemtodo];
+  todoItem.forEach((i) => {
+    i.style.display = "block";
+  });
+}
+//  read date
 function date() {
   var d = new Date();
-  console.log(d.getDate());
-  console.log(d.getTime());
-  console.log(d.getFullYear());
+  year.innerText = d.getFullYear();
+  month.innerText = d.getMonth() + 1;
+  day.innerText = d.getDate();
 }
